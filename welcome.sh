db4807092cf8a24b32ea4735854f03545700bbce
bruno <bruninho_rodriguesv@hotmail.com>
2018-04-24 17:03:20 -0300

    Modificado o arquivo welcome.sh acrescentado estrutura if

--- welcome.sh
+++ welcome.sh
@@ -1,5 +1,18 @@
 !#/bin/bash
 clear;
-echo -n "bruno";
+echo -n "digite seu nome: "; 
 read nome;
-echo "Hello "$nome;
+echo -n "DIgite a sua idade: ";
+read idade;
+clear;
+echo "____________________";
+echo "nomE: "$nome; 
+echo "idade: "$idade;
+if [ $idade -lt 18 ]; then
+echo "Menor de idade";
+else
+echo "maior de idade";
+fi
+echo "____________________";
+exit; 
+ 
